a315eb2e68050c0c838d80dad96e36ceafd6e178
jcagua <Jonathan.cagua@easymetering.com>
2023-03-01 19:02:43 -0500

    ADD: base proyecto

---
+++ pid_control/GNU ARM v10.3.1 - Default/gecko_sdk_4.2.1/platform/emdrv/tempdrv/src/tempdrv.d
@@ -0,0 +1,118 @@
+gecko_sdk_4.2.1/platform/emdrv/tempdrv/src/tempdrv.o: \
+ ../gecko_sdk_4.2.1/platform/emdrv/tempdrv/src/tempdrv.c \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\common\toolchain\inc/sl_gcc_preinclude.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/em_device.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22c224f512im40.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\CMSIS\Core\Include/core_cm33.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\CMSIS\Core\Include/cmsis_version.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\CMSIS\Core\Include/cmsis_compiler.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\CMSIS\Core\Include/cmsis_gcc.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\CMSIS\Core\Include/mpu_armv8.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/system_efr32bg22.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_emu.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_cmu.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_hfxo.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_hfrco.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_fsrco.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dpll.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_lfxo.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_lfrco.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ulfrco.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_msc.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_icache.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_prs.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpio.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpio_port.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldma.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldmaxbar.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_timer.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_usart.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_burtc.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_i2c.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_syscfg.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_buram.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpcrc.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dcdc.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_pdm.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_aes.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_smu.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_rtcc.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_letimer.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_iadc.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_wdog.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_eusart.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_cryptoacc.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_devinfo.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_prs_signals.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dma_descriptor.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldmaxbar_defines.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\autogen/sl_component_catalog.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emlib\inc/em_system.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emlib\inc/em_emu.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emlib\inc/em_bus.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\common\inc/sl_assert.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emlib\inc/em_core.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\common\inc/sl_common.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\common\inc/sl_assert.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\config/emlib_core_debug_config.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emdrv\tempdrv\inc/tempdrv.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emdrv\common\inc/ecode.h \
+ C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\config/tempdrv_config.h
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\common\toolchain\inc/sl_gcc_preinclude.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/em_device.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22c224f512im40.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\CMSIS\Core\Include/core_cm33.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\CMSIS\Core\Include/cmsis_version.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\CMSIS\Core\Include/cmsis_compiler.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\CMSIS\Core\Include/cmsis_gcc.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\CMSIS\Core\Include/mpu_armv8.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/system_efr32bg22.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_emu.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_cmu.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_hfxo.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_hfrco.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_fsrco.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dpll.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_lfxo.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_lfrco.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ulfrco.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_msc.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_icache.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_prs.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpio.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpio_port.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldma.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldmaxbar.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_timer.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_usart.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_burtc.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_i2c.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_syscfg.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_buram.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpcrc.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dcdc.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_pdm.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_aes.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_smu.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_rtcc.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_letimer.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_iadc.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_wdog.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_eusart.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_cryptoacc.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_devinfo.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_prs_signals.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dma_descriptor.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldmaxbar_defines.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\autogen/sl_component_catalog.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emlib\inc/em_system.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emlib\inc/em_emu.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emlib\inc/em_bus.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\common\inc/sl_assert.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emlib\inc/em_core.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\common\inc/sl_common.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\common\inc/sl_assert.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\config/emlib_core_debug_config.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emdrv\tempdrv\inc/tempdrv.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\gecko_sdk_4.2.1\platform\emdrv\common\inc/ecode.h:
+C:\Users\dev_fw\SimplicityStudio\v5_workspace\termometro\config/tempdrv_config.h:
---
+++ pid_control/app.c
@@ -0,0 +1,29 @@
+/***************************************************************************//**
+ * @file
+ * @brief Top level application functions
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+#include "blink.h"
+#include "sl_sensor_rht.h"
+/***************************************************************************//**
+ * Initialize application.
+ ******************************************************************************/
+void app_init(void)
+{
+  sl_status_t sc;
+  // Init temperature sensor.
+  sc = sl_sensor_rht_init();
+
+  blink_init();
+}
